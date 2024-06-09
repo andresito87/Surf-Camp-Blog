179f64f1eb670659d5a0b6390d012bc7a1c2d01f
Andres <andrespodadera87@gmail.com>
2024-06-09 17:34:59 +0200

    adding dinamic routing based in BD data

---
+++ surfcamp_frontend/app/blog/[article]/page.jsx
@@ -0,0 +1,25 @@
+import { fetchBlogArticles, fetchDataFromStrapi } from "@/utils/strapi.utils";
+
+export default async function Page({ params }) {
+  const { article: slug } = params;
+  const articles = await fetchBlogArticles();
+  // Find the article that matches the slug what was passed in the URL like /blog/my-article
+  const article = articles.find((article) => article.slug === slug);
+  return (
+    <main>
+      <h1>{article.headline}</h1>
+    </main>
+  );
+}
+
+// This function will be called at build time and will return an array of possible values for article
+// https://nextjs.org/docs/pages/building-your-application/data-fetching/get-static-props
+export async function getStaticParams() {
+  const articles = await fetchDataFromStrapi("blog-articles");
+
+  return articles.map((article) => ({
+    article: {
+      article: article.attributes.slug,
+    },
+  }));
+}
